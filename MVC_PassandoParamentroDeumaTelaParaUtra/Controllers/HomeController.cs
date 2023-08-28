3292ecd07756333052f15c8d0349a80a79d40eda
AlexFeitoza <AlexFeitoza@DESKTOP-5J5B422>
2023-08-27 21:07:55 -0300

    Add project files.

---
+++ MVC_PassandoParamentroDeumaTelaParaUtra/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_PassandoParamentroDeumaTelaParaUtra.Models;
+using System.Diagnostics;
+
+namespace MVC_PassandoParamentroDeumaTelaParaUtra.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult AlterarSenha(string login )
+        {
+            ViewBag.Login = login;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AlterarSenha(Usuario usuario)
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
